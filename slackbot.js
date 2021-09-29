a0f6b3801c712c13ac9719c4f7e375270b062af2
Vighneshwar Hegde <vighneshwar.meltota@gmail.com>
2021-09-29 23:35:44 +0530

    slackbot init

---
+++ slackbot.js
@@ -0,0 +1,26 @@
+const { WebClient } = require('@slack/web-api');
+
+// es6 import convention
+// import WebClient from '@slack/web-api';
+
+console.log('Getting started with Node Slack SDK');
+
+// Create a new instance of the WebClient class with the token read from your environment variable
+const web = new WebClient(process.env.SLACK_TOKEN);
+// The current date
+const currentTime = new Date().toTimeString();
+
+(async () => {
+
+  try {
+    // Use the `chat.postMessage` method to send a message from this app
+    await web.chat.postMessage({
+      channel: '#general',
+      text: `The current time is ${currentTime}`,
+    });
+    console.log('Message posted!');
+  } catch (error) {
+    console.log(error);
+  }
+
+})();
